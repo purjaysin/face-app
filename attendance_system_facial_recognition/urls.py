31b7fbd2695c5c585f14cbebf36dabc884d01836
Puranjay <purjaysin@outlook.com>
2022-05-21 22:02:59 +0530

    Important functional changes

--- attendance_system_facial_recognition/urls.py
+++ attendance_system_facial_recognition/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
     path('view_my_attendance', recog_views.view_my_attendance_employee_login ,name='view-my-attendance-employee-login'),
     path('not_authorised', recog_views.not_authorised, name='not-authorised'),
     path('hand_det',recog_views.index, name='hand-det'),
-    path('video',recog_views.video,name="video")
-
+    path('inattendance',recog_views.index_in,name='in-attendance'),
+    path('video',recog_views.video,name="video"),
+    path('video2',recog_views.video2,name="video2"),
+    path('action_done',recog_views.action,name="action-done")
 ]
